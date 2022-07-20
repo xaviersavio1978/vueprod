13a1db0b1bfba4d5bac41d1c065ec37bfadc27a6
Xavier Fernandes <xavierf@metlifecare.co.nz>
2022-07-20 15:26:04 +1200

    files

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World,this is just a test site using Azure Front Door - test "
+      value: "World,this is just a test site using Azure Front Door - test prod test "
     };
   }
 };
